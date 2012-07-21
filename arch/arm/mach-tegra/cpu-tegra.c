31b20be3d00b51885cb7faf7f3ab0c08416dce00
motley <motley.slate@gmail.com>
2012-07-21 00:28:38 -0400

    tegra3  dvfs/clock tweaks
    -Allow conditional GPU OC compile
    -Frequency and voltage tweaks
    -Hope to fix units that can't OC (CPU process_id = 3)
    -Tweak cputegra to see if we can get the CPU to settle back a bit
    -Tweak frequencies, some lower frequencies should be used more often for
    better battery
    Conflicts:
    
    	arch/arm/mach-tegra/cpu-tegra.c

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -330,6 +330,9 @@ static int tegra_cpu_edp_notify(
 
 		new_speed = edp_governor_speed(cpu_speed);
 
+<<<<<<< HEAD
+>>>>>>> e6d03d1... tegra3  dvfs/clock tweaks
+=======
 >>>>>>> e6d03d1... tegra3  dvfs/clock tweaks
 		if (new_speed < cpu_speed) {
 			ret = tegra_cpu_set_speed_cap(NULL);
